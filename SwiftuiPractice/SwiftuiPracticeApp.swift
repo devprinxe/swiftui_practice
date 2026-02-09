035ce871905e744653c5a062ba47f2a814c1a0e4
Prince Mahmud <46414838+devprinxe@users.noreply.github.com>
2026-02-09 14:32:17 +0600

    Initial Commit

---
+++ SwiftuiPractice/SwiftuiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiPracticeApp.swift
+//  SwiftuiPractice
+//
+//  Created by TechnoNext on 9/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
